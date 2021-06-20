ee5fb180675aee8057069eb52a2d1505aa631061
Qais Alshorman <qaishorman20@gmail.com>
2021-06-20 16:42:41 +0300

    modified repo

---
+++ index.js
@@ -0,0 +1,11 @@
+let Qus1=  prompt("what your feeling for this day") ;
+console.log("Qus1");
+
+let Qus2=  prompt("how is the weather today in your region") ;
+console.log("Qus2")
+
+let Qus3=  prompt("how to make you day a wonder full day") ;
+console.log("Qus3")
+
+let Qus4=  prompt("share the nice moment with us ") ;
+console.log("Qus4")
