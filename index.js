2427869c169eef7f86e3c8d5e23503badaa162fe
Qais Alshorman <qaishorman20@gmail.com>
2021-06-20 16:56:20 +0300

    modified

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-let Qus1=  prompt("what your feeling for this day") ;
-console.log("Qus1");
-
+let Qus1=  prompt("what your feeling for this day") 
+console.log(Qus1)
+alert("you enter",Qus1)
 let Qus2=  prompt("how is the weather today in your region") ;
-console.log("Qus2")
-
+console.log(Qus2)
+alert("you enter",Qus2)
 let Qus3=  prompt("how to make you day a wonder full day") ;
-console.log("Qus3")
-
+console.log(Qus3)
+alert("you enter",Qus3)
 let Qus4=  prompt("share the nice moment with us ") ;
-console.log("Qus4")
+console.log(Qus4)
+alert("you enter",Qus4)
